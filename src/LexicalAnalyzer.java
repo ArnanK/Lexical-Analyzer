918ae2959b476daf655009ff68bc9e3df8d561d7
ArnanK <arnank9@gmail.com>
2024-05-09 14:40:31 -0400

    initial

--- src/LexicalAnalyzer.java
+++ src/LexicalAnalyzer.java
@@ -1,20 +1,16 @@
 import java.util.HashMap;
+import java.io.File;
+
 
 public class LexicalAnalyzer {
-    private String[] reservedWords = {
-        "if",
-        "else",
-        "for",
-        "int",
-        "double",
-        "String",
-        "char",
-    };
-
+    
+    /* Token Objects */
     Token tokens[] = {
         new Token("SEMI_COLON"),
         new Token("INT_LIT"),
         new Token("DOUBLE_LIT"),
+        new Token("STRING_LIT"),
+        new Token("CHAR_LIT"),
         new Token("INDET"),
         new Token("ASSIGN_OP"),
         new Token("ADD_OP"),
@@ -29,13 +25,25 @@ public class LexicalAnalyzer {
         new Token("ELSE_STMT")
 
     };
-    
+
+
+
+    /* HashMap to store the relation between each lexeme with their respective Token 
+     * O(1) ~ Time Complexity
+    */
     private HashMap<String,Token> lexemsToTokens = new HashMap<String,Token>();
     
 
 
-    public LexicalAnalyzer(){
+    public LexicalAnalyzer(File file){
+
     }
+
+    private void getChar(){
+        
+    }
+
+
 
 
 
